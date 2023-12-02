c0c15c311f7a1ac6c5369b1ca8845b1153b57659
jmlira2002 <145620199+jmlira2002@users.noreply.github.com>
2023-12-01 23:46:33 -0600

    Update integradora.py

--- integradora.py
+++ integradora.py
@@ -35,7 +35,7 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-df = pd.read_csv("Police shortdata.xlsx")
+df = pd.read_excel("Police shortdata.xlsx")
 df['Incident Date'] = pd.to_datetime(df['Incident Date'])
 df['Incident Datetime'] = pd.to_datetime(df['Incident Datetime'])
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
